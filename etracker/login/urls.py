56e3e324abb2f670bae33365cb859451d4cbba2a
Eszter Toth <esztit@gmail.com>
2020-03-17 14:01:23 +0000

    Login works

--- etracker/login/urls.py
+++ etracker/login/urls.py
@@ -5,6 +5,5 @@ from django.contrib.auth.views import LoginView
 app_name = "login"
 
 urlpatterns = [
-    path("", LoginView.as_view(template_name="/login/index.html"), name="index")
-    #views.index, name="index"),
+    path("", LoginView.as_view(template_name="login/index.html")),
 ]
