054ccb0280d3aeaa5e95671fe700e26c641bb088
Eszter Toth <esztit@gmail.com>
2020-03-18 20:17:49 +0000

    Created ticket model

--- etracker/tickets/models.py
+++ etracker/tickets/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Ticket(models.Model):
+  status_choices = [
+    ("1", "New"),
+    ("2", "In Progress"),
+    ("3", "Resolved"),
+    ("4", "Closed"),
+  ]
+  priority_choices = [
+    ("1", "Low"),
+    ("2", "Medium"),
+    ("3", "High"),
+  ]
+  users = User.objects.all().values_list('username', flat=True)
+  assignee_choices = [
+    (num, name) for num, name in enumerate(users) if User.objects.get(username=name).is_active
+  ]
+
+  status = models.CharField(max_length=10, choices=status_choices)
+  priority = models.CharField(max_length=10, choices=priority_choices)
+  description = models.CharField(max_length=255)
+  details = models.TextField()
+  assignee = models.CharField(max_length=255, choices=assignee_choices)
+  date_added = models.DateTimeField(default=timezone.now)
+  date_updated = models.DateTimeField(blank=True, null=True)
+  added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+  def update(self):
+    self.date_updated = timezone.now()
+    self.save()
+
+  def __str__(self):
+    return self.status, self.priority, self.description, self.assignee
+
