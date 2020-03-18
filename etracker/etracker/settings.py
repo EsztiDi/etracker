00bca4874717d70008023f3251208bed0eb210cb
Eszter Toth <esztit@gmail.com>
2020-03-18 18:23:11 +0000

    Added ticket app

--- etracker/etracker/settings.py
+++ etracker/etracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tickets.apps.TicketsConfig',
     'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
