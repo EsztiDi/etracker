ae04617482deedc4710b92f5e35f7f2accb46f11
Eszter Toth <esztit@gmail.com>
2020-03-19 21:15:58 +0000

    Admin and Ticket works

--- etracker/tickets/admin.py
+++ etracker/tickets/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 from .models import Ticket
+from django.contrib.auth.models import User
 
 
 class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "description", "added_by",
+                    "date_updated", "date_added")
+
     fieldsets = (
         (None, {"fields": ("status", "priority",
-                           "description", "details", "assignee", "added_by")}),
+                           "description", "details", "assignee")}),
         ("Date Information",
             {"fields": ("date_added", "date_updated"), "classes": ("collapse",), }),)
-    readonly_fields = ("date_added", "date_updated", "added_by")
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.added_by = request.user
+        obj.save()
 
 
 admin.site.register(Ticket, TicketAdmin)
--- etracker/tickets/models.py
+++ etracker/tickets/models.py
@@ -1,45 +1,44 @@
 from django.db import models
-from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class Ticket(models.Model):
     status_choices = [
-        (0, "New"),
-        (1, "In Progress"),
-        (2, "Resolved"),
-        (3, "Closed"),
+        (1, "New"),
+        (2, "In Progress"),
+        (3, "Resolved"),
+        (4, "Closed"),
     ]
     priority_choices = [
-        (0, "Low"),
-        (1, "Medium"),
-        (2, "High"),
+        (1, "Low"),
+        (2, "Medium"),
+        (3, "High"),
     ]
 
     users = User.objects.all().values_list("username", flat=True)
 
     assignee_choices = [
-        (num, str(name)) for num, name in enumerate(users) if User.objects.get(username=str(name)).is_active
+        (num, str(name)) for num, name in enumerate(users, 1) if User.objects.get(username=str(name)).is_active
     ]
 
     status = models.IntegerField(
-        choices=status_choices, blank=False, default=0)
+        choices=status_choices, blank=False, default=1)
     priority = models.IntegerField(
-        choices=priority_choices, blank=False, default=0)
+        choices=priority_choices, blank=False, default=1)
     description = models.CharField(
         max_length=255, help_text="A short summary of the issue")
     details = models.TextField(verbose_name="More details")
     assignee = models.IntegerField(choices=assignee_choices)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(default=timezone.now)
     date_updated = models.DateTimeField(
-        default=date_added, verbose_name="Last updated")
+        null=True, blank=True, verbose_name="Last updated")
     added_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+        User, on_delete=models.PROTECT)
 
     def update(self):
         self.date_updated = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.status, self.priority, self.description, self.assignee, self.date_updated
+        return self.description
