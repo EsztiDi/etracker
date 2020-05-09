92bdef0ef47eab1eb7b6d2da2df3afad8372feb7
Eszter Toth <esztit@gmail.com>
2020-05-09 18:46:57 +0100

    Changed staticfiles_dir

--- etracker/etracker/settings.py
+++ etracker/etracker/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    "./static/",
+    "/home/etracker/etracker.eu.pythonanywhere.com/etracker/static/",
 ]
 
 LOGIN_REDIRECT_URL = "/home/"
